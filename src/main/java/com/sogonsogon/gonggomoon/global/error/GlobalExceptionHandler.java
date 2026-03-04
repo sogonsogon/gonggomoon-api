076689bfe8c38d2ff0b41776e61e272286e7bd9b
JSeHoone <beebea545@gmail.com>
2026-03-05 00:34:09 +0900

    refactor(global): GlobalExceptionHandler에 로그 코드 삭제
    - 추후에 로그를 도입할 때 적용하도록

--- src/main/java/com/sogonsogon/gonggomoon/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/sogonsogon/gonggomoon/global/error/GlobalExceptionHandler.java
@@ -9,18 +9,14 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j // 로깅을 위한 어노테이션 추가
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // 1. 우리가 만든 BaseException 처리
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<BaseResponse<?>> handleBaseException(BaseException e) {
-        String traceId = MDC.get("traceId");
-        BaseErrorCode errorCode = e.getErrorCode();
 
-        // 비즈니스 예외도 추적을 위해 로그를 남깁니다 (Warn 레벨 권장)
-        log.warn("[BaseException] traceId={}, code={}, message={}", traceId, errorCode.getCode(), errorCode.getMessage());
+        BaseErrorCode errorCode = e.getErrorCode();
 
         return ResponseEntity
             .status(errorCode.getStatus())
@@ -30,10 +26,6 @@ public class GlobalExceptionHandler {
     // 2. 예상하지 못한 에러 처리 (가장 중요 ⭐)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<BaseResponse<?>> handleException(Exception e) {
-        String traceId = MDC.get("traceId");
-
-        // e.printStackTrace() 대신 log.error 사용 (Loki로 스택 트레이스 전체 전송)
-        log.error("[UnhandledException] traceId={}, message={}", traceId, e.getMessage(), e);
 
         GlobalErrorCode error = GlobalErrorCode.INTERNAL_SERVER_ERROR;
 
@@ -45,7 +37,6 @@ public class GlobalExceptionHandler {
     // 3. @Valid 검증 실패 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<BaseResponse<?>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        String traceId = MDC.get("traceId");
 
         List<BaseResponse.ValidationError> errors = e.getBindingResult()
             .getFieldErrors()
@@ -57,12 +48,10 @@ public class GlobalExceptionHandler {
             )
             .toList();
 
-        log.warn("[MethodArgumentNotValidException] traceId={}, errors={}", traceId, errors);
-
         GlobalErrorCode errorCode = GlobalErrorCode.INVALID_INPUT_VALUE;
 
         return ResponseEntity
             .status(errorCode.getStatus())
-            .body(BaseResponse.fail(errorCode.name(), errorCode.getMessage(), errors /*, traceId*/));
+            .body(BaseResponse.fail(errorCode.name(), errorCode.getMessage(), errors));
     }
 }
