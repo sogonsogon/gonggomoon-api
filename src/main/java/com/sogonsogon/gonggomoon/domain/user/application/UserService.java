53d65dd21345df189ead44a4089fbb3d28a1a8fc
JSeHoone <beebea545@gmail.com>
2026-03-05 00:58:01 +0900

    feat(user): 내 정보 조회 API 구현
    - UserController에서 DTO를 만들어서 응답
    - UserService에서 findById로 User 객체를 리턴
    - UserJpaRepository로 변경함
    
    리# [예시] - Closes #123

--- src/main/java/com/sogonsogon/gonggomoon/domain/user/application/UserService.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/user/application/UserService.java
@@ -1,4 +1,25 @@
 package com.sogonsogon.gonggomoon.domain.user.application;
 
+import com.sogonsogon.gonggomoon.domain.user.application.exception.UserNotFoundException;
+import com.sogonsogon.gonggomoon.domain.user.domain.User;
+import com.sogonsogon.gonggomoon.domain.user.domain.UserRepository;
+import com.sogonsogon.gonggomoon.global.error.BaseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+    }
 }
