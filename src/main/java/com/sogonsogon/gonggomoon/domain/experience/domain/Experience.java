544ce64bf04b209922fa08f9522469975b55cbd0
juyeongMoon888 <whoor2828@gmail.com>
2026-03-04 15:44:18 +0900

    refactor(experience): AuditingEntityListener 적용

--- src/main/java/com/sogonsogon/gonggomoon/domain/experience/domain/Experience.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/experience/domain/Experience.java
@@ -2,6 +2,7 @@ package com.sogonsogon.gonggomoon.domain.experience.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -22,6 +24,7 @@ import java.time.LocalDateTime;
 @Table(name = "experience")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Experience {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
