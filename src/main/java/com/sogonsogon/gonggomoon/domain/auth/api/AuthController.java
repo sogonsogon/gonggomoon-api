b8ab438b3490640237d179b76ab59389ad633f3e
JSeHoone <beebea545@gmail.com>
2026-03-05 19:43:15 +0900

    feat(auth): 로그아웃 API 구현
    - AuthController : 서비스에서 제공하는 Authentication을 관리하는 컨트롤러
    - AuthService : 서비스에서 제공하는 Authentication을 관리하는 서비스
    - TokenService : RefreshToken 테이블을 관리하는 서비스

--- src/main/java/com/sogonsogon/gonggomoon/domain/auth/api/AuthController.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/auth/api/AuthController.java
@@ -1,4 +1,33 @@
 package com.sogonsogon.gonggomoon.domain.auth.api;
 
+import com.sogonsogon.gonggomoon.domain.auth.application.AuthService;
+import com.sogonsogon.gonggomoon.domain.auth.infrastructure.security.AccessUser;
+import com.sogonsogon.gonggomoon.global.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/logout")
+    public ResponseEntity<BaseResponse<Void>> logout(
+        @AuthenticationPrincipal AccessUser user,
+        @CookieValue(name = "refresh_token") String refreshToken
+        ) {
+        authService.logout(user.getId(), refreshToken);
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(BaseResponse.success());
+    }
 }
--- src/main/java/com/sogonsogon/gonggomoon/domain/auth/application/AuthService.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/auth/application/AuthService.java
@@ -1,4 +1,27 @@
 package com.sogonsogon.gonggomoon.domain.auth.application;
 
+import com.sogonsogon.gonggomoon.domain.auth.application.exception.RefreshTokenInvalidException;
+import com.sogonsogon.gonggomoon.domain.auth.infrastructure.jwt.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final TokenService tokenService;
+
+
+    @Transactional
+    public void logout(Long userId, String refreshToken) {
+
+        if (refreshToken == null || !jwtTokenProvider.validateToken(refreshToken)) {
+            throw new RefreshTokenInvalidException();
+        }
+
+        tokenService.revokeRefreshToken(userId, refreshToken);
+    }
 }
