382c693769d9901bd603c9f2656befb4b100a0f6
JSeHoone <beebea545@gmail.com>
2026-03-05 16:45:55 +0900

    feat(auth): OAuth로 로그인 완료 시, OAuthAccount 저장.

--- src/main/java/com/sogonsogon/gonggomoon/domain/auth/infrastructure/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/auth/infrastructure/oauth2/OAuth2SuccessHandler.java
@@ -1,6 +1,10 @@
 package com.sogonsogon.gonggomoon.domain.auth.infrastructure.oauth2;
 
+import com.sogonsogon.gonggomoon.domain.auth.domain.OAuthAccount;
+import com.sogonsogon.gonggomoon.domain.auth.domain.OAuthAccountRepository;
+import com.sogonsogon.gonggomoon.domain.auth.domain.OAuthProvider;
 import com.sogonsogon.gonggomoon.domain.auth.infrastructure.jwt.JwtTokenProvider;
+import com.sogonsogon.gonggomoon.domain.auth.infrastructure.security.AccessUser;
 import com.sogonsogon.gonggomoon.domain.auth.infrastructure.security.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.sogonsogon.gonggomoon.domain.auth.infrastructure.security.TokenCookieManager;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +13,9 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +26,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     @Value("${spring.security.oauth2.client.redirect-front-uri}")
     private String REDIRECT_URI;
 
+    private final OAuth2AuthorizedClientService authorizedClientService;
+    private final OAuthAccountRepository oauthAccountRepository;
+
     private final JwtTokenProvider tokenProvider;
     private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
     private final TokenCookieManager tokenCookieManager;
@@ -28,6 +38,37 @@ public void onAuthenticationSuccess(HttpServletRequest request,
                                     HttpServletResponse response,
                                     Authentication authentication) throws IOException {
 
+    /*
+    * OAuthAccount에 토큰을 저장하기 위한 로직
+    * */
+    // 1. AuthorizedClient를 통해 Refresh Token이 포함된 객체 획득
+    OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+    OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+        oauthToken.getAuthorizedClientRegistrationId(),
+        oauthToken.getName());
+
+    // 2. Refresh Token 추출
+    String providerAccessToken = client.getAccessToken().getTokenValue();
+    String providerRefreshToken = (client.getRefreshToken() != null)
+        ? client.getRefreshToken().getTokenValue()
+        : null;
+
+    AccessUser accessUser = (AccessUser) authentication.getPrincipal();
+    OAuthAccount newSocialUser = OAuthAccount.builder()
+        .userId(accessUser.getId())
+        .provider(OAuthProvider.from(oauthToken.getAuthorizedClientRegistrationId()))
+        .providerId(accessUser.getProviderId())
+        .accessToken(providerAccessToken)
+        .refreshToken(providerRefreshToken)
+        .build();
+
+    oauthAccountRepository.save(newSocialUser);
+
+
+    /*
+    * 유저에게 서비스(공고문)에서 발급하는 토큰 제공
+    * */
     String accessToken = tokenProvider.createAccessToken(authentication);
     String refreshToken = tokenProvider.createRefreshToken(authentication);
 
