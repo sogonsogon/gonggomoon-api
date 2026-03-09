dc0de9c7f2bcde2aa637292c1a893c1f76d2da74
sayhoon <103999840+JSeHoone@users.noreply.github.com>
2026-03-09 09:56:07 +0900

    test: CD 파이프 라인을 develop 브랜치에 머지되어도 동작하도록 수정 (#24)

--- src/test/java/com/sogonsogon/gonggomoon/GonggomoonApiApplicationTests.java
+++ src/test/java/com/sogonsogon/gonggomoon/GonggomoonApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.sogonsogon.gonggomoon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GonggomoonApiApplicationTests {
 
     @Test
