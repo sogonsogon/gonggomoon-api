098b8a8d4294cbaf56dcb3916dbe0930ba3cf3b7
JinYong833 <jjystudy1@gmail.com>
2026-03-10 19:48:59 +0900

    refactor[post] : 그냥 이름 변경

---
+++ src/main/java/com/sogonsogon/gonggomoon/domain/post/infrastructure/PostJpaRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.sogonsogon.gonggomoon.domain.post.infrastructure;
+
+import com.sogonsogon.gonggomoon.domain.post.domain.Post;
+import com.sogonsogon.gonggomoon.domain.post.domain.PostRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostJpaRepositoryImpl extends JpaRepository<Post, Long>, PostRepository {
+}
