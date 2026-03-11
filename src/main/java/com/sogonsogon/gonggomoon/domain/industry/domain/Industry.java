4eb118911d3eda756f5ef2f00ef6647dfb9bd8ba
JinYong833 <jjystudy1@gmail.com>
2026-03-11 17:05:53 +0900

    feat[post] : 공고 단일 조회 및 목록 조회 구현
    - 조회 response를 꾸미기 위해 필요한 Company, Industry 엔티티 구현
    - 공고 에러 추가
    - 공고 검색 파라미터 dto 생성

--- src/main/java/com/sogonsogon/gonggomoon/domain/industry/domain/Industry.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/industry/domain/Industry.java
@@ -1,4 +1,45 @@
 package com.sogonsogon.gonggomoon.domain.industry.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "industries")
 public class Industry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "created_by")
+    private Long createdBy;
+
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    protected Industry () {}
 }
--- src/main/java/com/sogonsogon/gonggomoon/domain/post/dto/request/SearchPostRequest.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/post/dto/request/SearchPostRequest.java
@@ -1,12 +1,11 @@
 package com.sogonsogon.gonggomoon.domain.post.dto.request;
 
-import com.sogonsogon.gonggomoon.domain.post.domain.PostStatus;
 import com.sogonsogon.gonggomoon.domain.strategy.domain.JobType;
 import jakarta.validation.constraints.Size;
 
 public record SearchPostRequest(
+
         JobType jobType,
-        PostStatus status,
 
         @Size(max = 50, message = "검색어는 50자 이하입니다.")
         String title
