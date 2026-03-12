268e417a00ef9334a2544140bf0314008d143688
JinYong833 <jjystudy1@gmail.com>
2026-03-12 18:27:52 +0900

    fix[post] : 응답 값에 공고 id와 원문 url 추가 요청에 대한 수정
    - 작업 중이던 Industry 관련 투척 ㅎㅎ

--- src/main/java/com/sogonsogon/gonggomoon/domain/strategy/domain/JobType.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/strategy/domain/JobType.java
@@ -2,5 +2,13 @@ package com.sogonsogon.gonggomoon.domain.strategy.domain;
 
 public enum JobType {
     FRONTEND, // 프론트엔드
-    BACKEND // 백엔드
+    BACKEND, // 백엔드
+    EMBEDDED,
+    DEVOPS,
+    DATA_ANALYSIS,
+    AI,
+    INFORMATION_SECURITY,
+    DESIGN,
+    PM_PO,
+    QA
 }
