c39975c1b10a461f6d8140999db3415d1b29a678
JinYong833 <jjystudy1@gmail.com>
2026-03-12 23:51:17 +0900

    fix[post] : 유저가 공고 게시를 요청 했을 때 엔드포인트 오타 수정, request 필드명 수정(API 명세서와 같게), 베이스url 검증, 세훈님에게 DB에 플랫폼 정보 업데이트요청

--- src/main/java/com/sogonsogon/gonggomoon/domain/post/dto/request/SubmitPostRequest.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/post/dto/request/SubmitPostRequest.java
@@ -9,7 +9,7 @@ public record SubmitPostRequest(
 
         @NotBlank(message = "URL은 필수입니다.")
         @Pattern(regexp = "^https?://.+", message = "올바른 형식의 URL이 아닙니다.")
-        String requestUrl,
+        String postUrl,
 
         @NotNull(message = "플랫폼 ID는 필수입니다.")
         @Positive(message = "올바른 플랫폼 ID가 아닙니다.")
