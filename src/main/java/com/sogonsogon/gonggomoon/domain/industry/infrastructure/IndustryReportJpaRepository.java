198848fc042c485b89ee79bb8c6046bc68263749
JinYong833 <jjystudy1@gmail.com>
2026-03-13 11:49:06 +0900

    feat[industry] : 특정 산업의 발행된 보고서 조회

--- src/main/java/com/sogonsogon/gonggomoon/domain/industry/infrastructure/IndustryReportJpaRepository.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/industry/infrastructure/IndustryReportJpaRepository.java
@@ -5,10 +5,6 @@ import com.sogonsogon.gonggomoon.domain.industry.domain.IndustryReportRepository
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface IndustryReportJpaRepository extends JpaRepository<IndustryReport, Long>, IndustryReportRepository {
-
-    Optional<IndustryReport> findById(Long id);
 }
