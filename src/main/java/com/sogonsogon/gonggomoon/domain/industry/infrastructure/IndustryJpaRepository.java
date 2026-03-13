cc2d0702c0ff0ea8f0f2e1ee425e5314e41904c8
JinYong833 <jjystudy1@gmail.com>
2026-03-13 11:48:29 +0900

    feat[industry] : 산업 목록 조회

--- src/main/java/com/sogonsogon/gonggomoon/domain/industry/infrastructure/IndustryJpaRepository.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/industry/infrastructure/IndustryJpaRepository.java
@@ -5,10 +5,6 @@ import com.sogonsogon.gonggomoon.domain.industry.domain.IndustryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface IndustryJpaRepository extends JpaRepository<Industry, Long>, IndustryRepository {
-
-    Optional<Industry> findById(Long id);
 }
