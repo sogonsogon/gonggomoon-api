8f1812d8ce3f79ce0d8a8cc9f3be173f98e1fb51
JinYong833 <jjystudy1@gmail.com>
2026-03-14 15:37:48 +0900

    hoy[industry] : 리스폰스 형태 통일

--- src/main/java/com/sogonsogon/gonggomoon/domain/industry/api/IndustryController.java
+++ src/main/java/com/sogonsogon/gonggomoon/domain/industry/api/IndustryController.java
@@ -2,6 +2,8 @@ package com.sogonsogon.gonggomoon.domain.industry.api;
 
 import com.sogonsogon.gonggomoon.domain.industry.application.IndustryService;
 import com.sogonsogon.gonggomoon.domain.industry.dto.response.IndustryListResponse;
+import com.sogonsogon.gonggomoon.global.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,8 @@ public class IndustryController {
     }
 
     @GetMapping
-    public IndustryListResponse getIndustries() {
+    public ResponseEntity<BaseResponse<IndustryListResponse>> getIndustries() {
 
-        return industryService.getIndustries();
+        return ResponseEntity.ok(BaseResponse.success(industryService.getIndustries()));
     }
 }
