f37d69ff294e2858891db3959b5b9f179b561110
JinYong833 <jjystudy1@gmail.com>
2026-07-02 21:53:30 +0900

    feat[Post] : tavily api 적용
    - 요청 및 결과를 여러개 수행할 수 있게끔 적용
    - 환경설정 해야함 (api-key 등)

---
+++ src/main/java/com/sogonsogon/gonggomoon/domain/post/dto/request/TavilyExtractRequest.java
@@ -0,0 +1,12 @@
+package com.sogonsogon.gonggomoon.domain.post.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record TavilyExtractRequest(
+        @JsonProperty("api_key") String apikey,
+        List<String> urls,
+        @JsonProperty("extract_depth") String extractDepth
+) {
+}
